f8b257f60a58d62e555ded9f46e1675bdd00a0d3
yang12310 <yang12310@naver.com>
2023-05-13 12:50:16 +0900

    2023-05-13 02 commit

---
+++ src/components/MenuBar.js
@@ -0,0 +1,27 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function MenuBar() {
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Todolist
+          </Typography>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Search</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
